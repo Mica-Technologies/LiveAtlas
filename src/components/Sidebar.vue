5c188a918e60167b1c4ccb5f92743c449e4a0dc5
mica-alex <83238954+mica-alex@users.noreply.github.com>
2026-04-30 14:12:24 -0400

    Styling improvements more like MUI

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -215,8 +215,8 @@ export default defineComponent({
 			width: var(--ui-button-size);
 			height: var(--ui-button-size);
 			box-shadow: var(--box-shadow);
-			backdrop-filter: blur(24px);
-			-webkit-backdrop-filter: blur(24px);
+			backdrop-filter: blur(24px) saturate(1.2);
+			-webkit-backdrop-filter: blur(24px) saturate(1.2);
 			border: 1px solid var(--border-color);
 
 			& + button {
